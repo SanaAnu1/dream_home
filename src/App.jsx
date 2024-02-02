73f2c65c0f881817bd3e46c3a7677303ac4d8059
Sana Aboobacker <sanaaboobackerk@gmail.com>
2024-02-02 20:44:05 +0400

    dreamHome

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import SignIn from './pages/Signin'
+import SignUp from './pages/SignUp'
+import Header from './components/Header'
+
 
 function App() {
   return (
-    <div><h1 className='text-red-800'>App</h1></div>
+    <div>
+      <BrowserRouter>
+      <Header/>
+        <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/sign-in' element={<SignIn/>}/>
+        <Route path='/sign-up' element={<SignUp/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/about' element={<About/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { FaSearch } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <div className='w-100'>
+<Navbar expand="lg" className="bg-red-800 border-bottom p-2" sticky='top'>
+      <Container>
+        <Navbar.Brand><Link to='/' className='text-decoration-none'><span className='text-violet-200 text-2xl'><i className="fa-solid fa-house fa-fade" style={{color:"#B197FC"}}></i><b> Dream</b></span><span className='text-violet-400 text-2xl'><b>Home</b></span></Link></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+        <Nav.Link className='me-auto'><form className='flex items-center'>
+              <input className='rounded-start p-1 ps-2 bg-violet-100 focus:outline-none' type="text" placeholder='Search'/>
+              <div className='bg-violet-100 p-2 rounded-end '><FaSearch /></div>
+            </form></Nav.Link>
+          <Nav className="ms-auto">
+          <Nav.Link><Link to='/' className='text-decoration-none'><span className='text-violet-200 hover:underline'>Home</span></Link></Nav.Link>
+          <Nav.Link><Link to='/about' className='text-decoration-none'><span className='text-violet-200 hover:underline'>About</span></Link></Nav.Link>
+            <Nav.Link><Link to='/sign-in'><button className='bg-violet-200 rounded px-2 py-1 hover:bg-violet-400'>Sign In</button></Link></Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </div>
+  )
+}
+
+export default Header
